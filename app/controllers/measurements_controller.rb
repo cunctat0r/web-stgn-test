453181956dd6d6a64f74c723508c7e26caa59fe8
Slava Petrov <slavik--petrov@yandex.ru>
2018-05-02 08:50:06 +0000

    Add devise

--- app/controllers/measurements_controller.rb
+++ app/controllers/measurements_controller.rb
@@ -1,2 +1,18 @@
 class MeasurementsController < ApplicationController
+  def index
+  end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @measurement = @post.measurements.create(measurement_params)
+    redirect_to post_path(@post)
+  end
+
+private
+  def measurement_params
+    params.require(:measurement).permit(
+      :F0, :T0, :Upit
+    )
+  end
+
 end
